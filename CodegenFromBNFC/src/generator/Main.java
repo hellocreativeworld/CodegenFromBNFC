40055725d0fc5612e11bee6427b30f90252f7315
Mary <compilerbau@ist-einmalig.de>
2016-06-28 12:11:22 +0200

    Ergaenzt um Ausgabe->Abst-Syntaxbaum und Ausgabe->Linearer-Baum aus Test

--- CodegenFromBNFC/src/generator/Main.java
+++ CodegenFromBNFC/src/generator/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import CPP.PrettyPrinter;
 import CPP.Yylex;
 import CPP.parser;
 import CPP.Absyn.Program;
@@ -11,38 +12,64 @@ import CPP.Absyn.Program;
 
 public class Main {
 
-	static  String filename;
+	public static  String filename;
+
 	public static void main(String[] args) {
-		
-		try {
-			
-			
-			File f = new File("testfiles_cc/good07.cc");
-			f.createNewFile();
+
+		try 
+		{
+
+			File f = new File("testfiles_cc/good01.cc");
+			System.out.println("f="+f.toString());
+			boolean testf = f.createNewFile();
+			System.out.println("File exists?"+!testf);
 			filename = f.getName();
-			if(!f.exists()){
-			System.out.println("File does not exist");
+			System.out.println("filename="+filename);
+
+			if(!f.exists())
+			{
+				System.out.println("File does not exist");
 			}
+
 			Yylex l = new Yylex(new FileReader(f));
-			
+
 			parser p = new parser(l);
+
+
+			try 
+			{
+				CPP.Absyn.Program parse_tree = p.pProgram();
+
+				System.out.println();
+				System.out.println("Parse Succesful!");
+				System.out.println();
+				System.out.println("[Abstract Syntax]");
+				System.out.println();
+				System.out.println(PrettyPrinter.show(parse_tree));
+				System.out.println();
+				System.out.println("[Linearized Tree]");
+				System.out.println();
+				System.out.println(PrettyPrinter.print(parse_tree));
+
+
+				generateCode(parse_tree);
 				
-				CPP.Absyn.Program parse_tree;
-				try {
-					parse_tree = p.pProgram();
-					generate(parse_tree);
-				} catch (Exception e) {
-					
-					System.out.println(e);
-				}
-				
+			} catch (Exception e) 
+			{
+				// Error-Message
+				System.out.println(e);
+			}
+
 		}catch(IOException e){
 			System.out.println(e.getMessage());
 		}
 
 	}
-	
+
 	/**
+	 * Codegeneration:
+	 * Durchlaufe den Abstrakten Syntaxbaum und 
+	 * traverse through the abstract syntax tree and 
 	 * 
 	 * implement the functions
 	 * 			-read through the doc of llvm to find the correct string values for the functions
@@ -54,25 +81,28 @@ public class Main {
 	 * maybe it does make sense to do sysos in every visit and eval 
 	 * @param parse_tree
 	 */
-	
-	private static void generate(Program parse_tree) {
+
+	private static void generateCode(Program parse_tree) {
+
 		//Compiler c = new Compiler();
-		System.out.println(parse_tree.toString());
-		
+		//System.out.println(parse_tree.toString());
+
 		/**
 		 * es wird die Version von eval aufgerufen, die der Parameter angibt.
 		 * Hier ist der Parameter Program (parse_tree ist vom Typ Program), 
 		 * also wird in der Klasse Compiler, die Methode eval(Program) ausgerufen
 		 */
 		Compiler.eval(parse_tree);
-		
-		
-		
+
+
 		System.out.println("Modul: LLVM-Output="+Module.llvm_output);
 		//Module.mod.dump();
-		Filewriter.writeFile(filename);
+
+		/**
+		 * filename ist static, deshalb wird er innerhalb von 
+		 * Filewriter mittels Main.filename aufgerufen
+		 */
+		Filewriter.writeFile();
 	}
-	
-	
 
 }
