edb76be24cba14115d022bc8ccc3eefe0093b33b
Mary <compilerbau@ist-einmalig.de>
2016-06-28 12:55:18 +0200

    um Kommentare ergaenzt

--- CodegenFromBNFC/src/generator/Module.java
+++ CodegenFromBNFC/src/generator/Module.java
@@ -7,7 +7,18 @@ public  class Module {
 	static HashMap<String, String> context = new HashMap<>(); // String funktionsmname String inhalt(llvm)
 	
 	
-	//TODO Stringbuilder
+	/**
+	 * Erstellt den LLVM-Code, also die LLVM IR des C++-Programms:
+	 * 
+	 * in jedem Knoten des Syntaxbaums wird die Funktion aufgerufen
+	 * und die entsprechende LLVM-Anweisung an den bestehenden LLVM-Code
+	 * angefuegt
+	 * 
+	 * Nach Programm-Ende steht in llvm_output die LLVM IR 
+	 * des C++ Programms
+	 * 
+	 * @param str der anzuhaengende String
+	 */
 	static void builder(String str){
 		StringBuilder sb = new StringBuilder();
 		sb.append(llvm_output).append(str);
